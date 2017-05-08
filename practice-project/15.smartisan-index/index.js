5968285547893abd5dc97d4e5d660d7116c738e6
zhaoyangcong <congzhaoyang1@163.com>
2017-05-08 23:53:22 +0800

    change swiper

--- practice-project/15.smartisan-index/index.js
+++ practice-project/15.smartisan-index/index.js
@@ -1,8 +1,10 @@
-    var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
-        paginationClickable: true,
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
+        paginationClickable: true,
         spaceBetween: 30,
-        effect: 'fade'
-    });
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+});
