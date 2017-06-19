b397f35ec6f2947c2c7b10889594257126aaaaf3
congzhaoyang <congzhaoyang1@163.com>
2017-06-19 15:09:49 +0800

    add learn require.js demo

---
+++ 25.require.js-demo/www/js/app/main.js
@@ -0,0 +1,6 @@
+define(["jquery", "jquery.alpha", "jquery.beta"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+        $('#content').html('hello world');
+    });
+});
